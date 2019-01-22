56b739a42bfd7e8159630c62aefb51bef46ba8d4
Roy van Rijn <r.rijn@portofrotterdam.com>
2019-01-22 20:09:43 +0100

    Ex2: Done

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
@@ -4,6 +4,16 @@ import com.paulienvanalst.rugbymatch.game.SetPiece
 import com.paulienvanalst.rugbymatch.team.TeamName
 import org.springframework.context.ApplicationEvent
 
-sealed class SetPieceEvent(source: Any, val setPiece: SetPiece, val winningTeam: TeamName) : ApplicationEvent(source)
+sealed class SetPieceEvent(source: Any, open val setPiece: SetPiece, open val winningTeam: TeamName) : ApplicationEvent(source)
 
+class ScrumWasPlayed(source: Any, val scrum: SetPiece, override val winningTeam: TeamName) : SetPieceEvent(source, scrum, winningTeam)
 
+class LineOutWasPlayed(source: Any, val lineOut: SetPiece, override val winningTeam: TeamName) : SetPieceEvent(source, lineOut, winningTeam)
+
+fun List<SetPieceEvent>.wonBy(teamName: TeamName) = this.filter { event -> event.winningTeam == teamName }
+
+fun List<SetPieceEvent>.lostBy(teamName: TeamName) = this.filter { event -> event.winningTeam != teamName }
+
+fun List<SetPieceEvent>.scrumEvents() = this.filter { event -> event is ScrumWasPlayed }
+
+fun List<SetPieceEvent>.lineOutEvents() = this.filter { event -> event is LineOutWasPlayed }
